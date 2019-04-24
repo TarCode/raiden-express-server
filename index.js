67d0f3b69d91515f6d9feb288115afa8afd65204
TarCode <tarcode33@gmail.com>
2019-04-24 21:57:15 +0200

    added GET tokens endpoint

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ require('dotenv').config()
 const express = require('express')
 const fetch = require('node-fetch')
 const cors = require('cors')
+var bodyParser = require('body-parser')
+const Maker = require('@makerdao/dai');
+
 const app = express()
 const port = 3001
 
 app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/address', async (req, res) => {
     try {
         const response  = await fetch(process.env.RAIDEN_CLIENT_URL + '/api/v1/address')
+        const json = await response.json();
+        res.send(json)
+    } catch(err) {
+        res.status(400).send({err});
+    }
+})
+
+app.get('/tokens', async (req, res) => {
+    try {
+        const response  = await fetch(process.env.RAIDEN_CLIENT_URL + '/api/v1/tokens')
         const json = await response.json();
         res.send(json)
     } catch(err) {
@@ -27,6 +41,48 @@ app.get('/channels', async (req, res) => {
     } catch(err) {
         res.status(400).send({err});
     }
+})
+
+app.post('/send', async (req, res) => {
+   const {amount, recipient_address} = req.body;
+
+   try {
+    const maker = await Maker.create('http',{
+        privateKey: process.env.PRIVATE_KEY,
+        url: process.env.INFURA_URL,
+        provider: {
+            type: 'HTTP', // or 'TEST'
+            network: 'kovan'
+        },
+        web3: {
+        statusTimerDelay: 2000,
+        confirmedBlockCount: 8,
+        transactionSettings: {
+            gasPrice: 12000000000
+        }
+        },
+     })
+
+    await maker.authenticate();
+
+    // const txMgr = maker.service('transactionManager');
+    
+    const cdp = await maker.openCdp();
+
+    await cdp.lockEth(amount);
+    await cdp.drawDai(parseInt(amount)* 100);
+
+    const dai = await maker.service('token').getToken('DAI');
+
+    // TODO: Send to contract via Raiden network
+
+    // TODO: Add txMgr listener to listen for DAI payments
+
+    dai.transfer(recipient_address, Maker.DAI(amount * 100));
+    return res.send({ result: "DAI Transferred"})
+   } catch (err) {
+       return res.send({err})
+   }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
